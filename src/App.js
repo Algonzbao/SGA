96923dc0400ff5a580db917381c5b711f9880c80
Algonzbao <56605609+Algonzbao@users.noreply.github.com>
2022-02-28 19:40:26 +0100

    Set Up básico entorno de react
    Aquí está todo funcionando normal en el puerto 3000

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Parrafo from './Componentes/Parrafo';
+import Variables from './Componentes/Variables';
+
+function App(){
+    return (
+        <div>
+            <h1> Hola mundo </h1>
+            <Parrafo />
+            <Variables />
+        </div>
+    )
+}
+
+export default App;
