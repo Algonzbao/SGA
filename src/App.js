6ea3fd94afe3c131b3ca617584066793f4600407
Algonzbao <56605609+Algonzbao@users.noreply.github.com>
2022-03-01 19:39:14 +0100

    Añadidos eventos y componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Parrafo from './Componentes/Parrafo';
 import Variables from './Componentes/Variables';
+import Eventos from './Componentes/Eventos';
+import Botones from './Componentes/Botones';
 
 function App(){
     return (
         <div>
-            <h1> Hola mundo </h1>
+            <h1 className='Titulo'> Hola mundo </h1>
             <Parrafo />
+            <Botones />
             <Variables />
+            <Eventos />
+            
         </div>
-    )
+    )  
 }
 
 export default App;
--- src/Componentes/Variables.jsx
+++ src/Componentes/Variables.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Variables = () => {
-  return (
+    const saludo = 'Hola desde constante'; //Crear una constante con un valor y luego llamarla
+    const imagen = 'https://geografiaehistoria.ucm.es/data/cont/docs/5-2018-04-11-5-portada-1%20(1).jpg'
+    return (
     <div>
-        <h2>Nuevo componente</h2>
+        <h2>Nuevo componente {saludo}</h2> 
+        <img src= {imagen} alt= ""/>
     </div>
   )
 }
