c3b4a03afbe563643657514af335a2c76cafd4a7
Algonz <algonz22@ucm.es>
2022-03-02 16:37:24 +0100

    Gitignore y botón con cambio dinámico
    primer useState

--- SGA/src/Componentes/Eventos.jsx
+++ SGA/src/Componentes/Eventos.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Eventos = () => {
+
+    const [texto, setTexto] = useState('Texto desde estado')
+
     const eventoClick = () => {
         console.log('he clicado')
+        setTexto('Cambio de texto')
     }
   return (
     <div>
         <hr/>
-        <h2> Eventos </h2>
+        <h2>{texto}  </h2>
     <button onClick = { () => eventoClick()}> Click </button>
     </div>
   )
