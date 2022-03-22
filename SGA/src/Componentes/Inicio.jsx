5f04f5e2c9b653381fc77860e4e3f8b896fdd496
Alf_on <alfnav01@ucm.es>
2022-03-22 16:34:13 +0100

    Inicio
    Inicio 1 foto

--- SGA/src/Componentes/Inicio.jsx
+++ SGA/src/Componentes/Inicio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Aula from './Aulas';
 import Reservar from './Reservar';
 
@@ -7,25 +8,43 @@ import {
   Link,
 } from "react-router-dom";
 
-const Inicio = () => {
+const Welcome = 'Portal de consulta de aulas.';
+
+
+/*
+function tick() {
+       {new Date().toLocaleTimeString()}
+  ReactDOM.render(document.getElementById('root'));
+}
+setInterval(tick, 1000);
+/*
+
+/*
+function Felcome(props) {  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Felcome name="Sara" />;ReactDOM.render(
+  element,
+  document.getElementById('root')
+); */
+
+
+
+function Inicio() {
   return (
     <div>
-      <div className="container bloqueFotos" >
+      <h1 className="text-light" classNam= "mt-5"> {Welcome} </h1>
+      <div id="container_bloqueFotos">
         <div className='btn-group'>
-          <div className="derecha">
+          <div id ="derecha" className= "mt-5">
             <Link to="/aula">
-              <img src="https://firebasestorage.googleapis.com/v0/b/sga-ghis.appspot.com/o/IMG_0326.jpg?alt=media&token=9b2b22b0-97c4-4852-b4d2-6220ba7b2d88" width="30%" />
-            </Link>
-          </div>
-          <div className='izquierda'>
-            <Link to="/Reservar" >
-              <img src="https://firebasestorage.googleapis.com/v0/b/sga-ghis.appspot.com/o/ordenador.jpg?alt=media&token=1f73ab38-0a79-4556-8573-d55e18743d40" width="30%" />
+              <img src="https://firebasestorage.googleapis.com/v0/b/sga-ghis.appspot.com/o/IMG_0326.jpg?alt=media&token=9b2b22b0-97c4-4852-b4d2-6220ba7b2d88" width="100%" />
             </Link>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Inicio
